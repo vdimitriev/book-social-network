aa0f6fd8a44c0cc71431546954c7c8591758bc9b
Vlado Dimitriev <vdimitriev@proton.me>
2025-01-18 17:12:03 +0100

    Add new spring boot project book-network-api.

---
+++ book-network/src/main/java/mk/dmt/bsn/book/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package mk.dmt.bsn.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
