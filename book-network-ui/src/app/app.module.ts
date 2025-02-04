630cc63cf2ecd6e1f91afa7bccca408de04c584f
vdimitriev <vdimitriev@proton.me>
2025-02-04 19:44:39 +0100

    Add book-network-ui angular module (subproject).

---
+++ book-network-ui/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from '@angular/forms';
+import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ActivateAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    HttpClient
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activate-account',
+  standalone: false,
+  
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss'
+})
+export class ActivateAccountComponent {
+
+}
